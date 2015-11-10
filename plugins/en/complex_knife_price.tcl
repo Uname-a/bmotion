05dfc63217c9efc21b96beea15a738182a532195
Casey Bartlett <caseytb@bu.edu>
2015-11-10 11:46:11 -0500

    knife_price trigger
    triggers on simple "price" - knife no longer required

--- plugins/en/complex_knife_price.tcl
+++ plugins/en/complex_knife_price.tcl
@@ -7,11 +7,11 @@
 # in the modules directory.
 ###############################################################################
 
-bMotion_plugin_add_complex "knife_price" "%botnicks,?:? (knife) (price|cost|value) (of) (.+)" 100 bMotion_plugin_complex_knife_price "en"
+bMotion_plugin_add_complex "knife_price" "%botnicks,?:? (price|cost|value) (of) (.+)" 100 bMotion_plugin_complex_knife_price "en"
 
 proc bMotion_plugin_complex_knife_price { nick host handle channel text } {
   global botnicks
-  if {[regexp -nocase "^${botnicks},?:? (knife) (price|cost|value) (of) (.+)" $text matches botn noparse article mhhmmm details]} {
+  if {[regexp -nocase "^${botnicks},?:? (price|cost|value) (of) (.+)" $text matches botn noparse details]} {
 		set aknife "[string range $details [expr [string first " " $details] + 1] [string length $details]]"
     bMotionDoAction $channel $aknife "I don't know how to look up the value of $aknife .... yet"
     return 1
